27c8d493f0fe09c64838d8ac4bc559917118c919
Yuval Langer <yuval.langer@gmail.com>
2013-12-12 01:44:55 +0200

    Fixing databases

--- src/wordwiki/settings.py
+++ src/wordwiki/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'wordwiki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-#DATABASES = {
+DATABASES = dict()
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
