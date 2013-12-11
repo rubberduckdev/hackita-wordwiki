452106f078b1ca73d437320cf081c2400a8c8e06
Yuval Langer <yuval.langer@gmail.com>
2013-12-12 00:34:06 +0200

    urls.py holds some dummy routings

--- src/wordwiki/urls.py
+++ src/wordwiki/urls.py
@@ -3,10 +3,20 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.http import HttpResponse
+
+def main(request):
+    return HttpResponse("Poop.")
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wordwiki.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', main, name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^word/[a-z]+$', '', name='pages'),
+    url(r'^word/[a-z]+/edit', '', name='pages'),
+    url(r'^add-word/', '', name='pages'),
+    url(r'^word/$', '', name='pages'),
 )
