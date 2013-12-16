d4a89ebe551293197ae5126ff3f102e298456c25
Yuval Langer <yuval.langer@gmail.com>
2013-12-16 10:46:50 +0200

    'from django.models' -> 'from django.http'

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,12 @@
 # Create your views here.
 
-from django.models import HttpResponse
+from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 from pages.models import Page
 
 def index(request):
-    pages = Poll.objects.all()
+    pages = Page.objects.all()
     template = loader.get_template('pages/index.html')
     context = RequestContext(
         request,
