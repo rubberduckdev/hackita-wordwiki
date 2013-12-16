0085ccd72e7e2d394dd21d6f88142041ffdab5d1
Yuval Langer <yuval.langer@gmail.com>
2013-12-16 14:11:35 +0200

    pages views

--- pages/urls.py
+++ pages/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls import patterns, url
 from pages import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^word/$', views.index, name='index'),
 )
