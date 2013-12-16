71da9d8f58fa6501eacd06b266abda66cb0b9a09
Yuval Langer <yuval.langer@gmail.com>
2013-12-16 09:26:17 +0200

    Adding a PageAdmin classw

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from pages.models import Page
 
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    """ Page's ModelAdmin """
+    fields = ['name', 'cotents']
+
+admin.site.register(Page, PageAdmin)
